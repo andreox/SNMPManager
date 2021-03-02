e3591d0a80f39f770acc33b3f8ea4128822fa28b
andreox <andreozzi.alessio@gmail.com>
2021-03-02 16:25:28 +0100

    now user is able to configure GET operations

--- GETThread.java
+++ GETThread.java
@@ -5,47 +5,27 @@ import java.util.Vector;
 import java.util.concurrent.*;
 import java.io.IOException;
 import java.lang.Thread;
+import java.util.ArrayList ;
 
-import org.snmp4j.CommandResponder;
-import org.snmp4j.CommandResponderEvent;
 import org.snmp4j.CommunityTarget;
-import org.snmp4j.MessageDispatcher;
-import org.snmp4j.MessageDispatcherImpl;
-import org.snmp4j.MessageException;
 import org.snmp4j.PDU;
-import org.snmp4j.PDUv1;
 import org.snmp4j.Snmp;
-import org.snmp4j.mp.MPv1;
-import org.snmp4j.mp.MPv2c;
-import org.snmp4j.mp.StateReference;
-import org.snmp4j.mp.StatusInformation;
-import org.snmp4j.security.Priv3DES;
-import org.snmp4j.security.SecurityProtocols;
 import org.snmp4j.smi.OctetString;
-import org.snmp4j.smi.TcpAddress;
-import org.snmp4j.smi.TransportIpAddress;
-import org.snmp4j.smi.UdpAddress;
-import org.snmp4j.transport.AbstractTransportMapping;
-import org.snmp4j.transport.DefaultTcpTransportMapping;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
-import org.snmp4j.util.MultiThreadedMessageDispatcher;
-import org.snmp4j.util.ThreadPool;
-import org.snmp4j.smi.OID;
 import org.snmp4j.mp.SnmpConstants ;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.event.ResponseEvent;
-import org.snmp4j.event.ResponseListener;
 
 public class GETThread implements Runnable {
 
 		private UdpAddress address ;
-		private OID oid ;
+		private ArrayList<OID> oid ;
 		private boolean stop_running ;
 		private CommunityTarget target ;
 		private Snmp s ;
 		private PDU pdu ;
 		
-		GETThread( UdpAddress address, OID oid ) throws IOException {
+		GETThread( UdpAddress address, ArrayList<OID> oid ) throws IOException {
 			
 			this.address = address ;
 			this.oid = oid ;
@@ -61,7 +41,8 @@ public class GETThread implements Runnable {
         	
     		pdu = new PDU();
     		pdu.setType(PDU.GET);
-    		pdu.add(new VariableBinding(this.oid));
+    		for ( int i = 0 ; i < oid.size() ; i++ ) 
+    			pdu.add(new VariableBinding(this.oid.get(i)));
 
         	//s.listen() ;
         	//manager.sendRequest( s , manager.createGetPdu(systime), target ) ;
@@ -76,7 +57,7 @@ public class GETThread implements Runnable {
 					s.listen();
 					ResponseEvent responseEvent = s.send(pdu, target);
 					PDU response = responseEvent.getResponse();
-					//System.out.println("I'm here - Thread Get") ;
+					System.out.println("I'm here - Thread Get ADDRESS "+address) ;
 					
 					if (response == null) {
 						
@@ -87,7 +68,7 @@ public class GETThread implements Runnable {
 						if (response.getErrorStatus() == PDU.noError) {
 							
 							Vector<? extends VariableBinding> vbs = response.getVariableBindings();
-							System.out.println("GET RESULT from IP " +address+" : ") ;
+							System.out.println("GET RESULT "+address+"  : ") ;
 							
 							for (VariableBinding vb : vbs) {
 								
@@ -112,7 +93,7 @@ public class GETThread implements Runnable {
 				}
 				
 			}
-			System.out.println("TRAP Thread stopped me");	
+			System.out.println("GET Thread Stopped running") ;
 		}
 		
 		public void stop_run() { this.stop_running = true ; }
--- ManagerServerExecutor.java
+++ ManagerServerExecutor.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List; 
+import java.util.ArrayList ;
 import java.util.concurrent.* ;
 import org.snmp4j.smi.UdpAddress;
 import org.snmp4j.smi.OID;
@@ -10,18 +12,35 @@ public class ManagerServerExecutor {
 
 	private HashMap <String,String> OID_Text_Mapping ; //Stringa descrittiva associata ad OID
 	private HashMap <String,String> OID_OIC_Mapping ;
+	private HashMap <UdpAddress, ArrayList<OID>> IP_OID_Get_Mapping ;
+	
 	
 	public static void main( String[] args ) throws IOException {
 		
-		ExecutorService executor = Executors.newFixedThreadPool(2) ;
-		UdpAddress udp_get = new UdpAddress("10.169.1.204/161") ;
-		OID oid_get = new OID("1.3.6.1.2.1.1.3.0") ; //sysUpTime
-		GETThread get = new GETThread( udp_get,  oid_get) ;
+		ManagerServerExecutor mse = new ManagerServerExecutor() ;
+		HashMap<UdpAddress, ArrayList<OID> > map = mse.getIP_OID_Get_Mapping();
+		ArrayList<GETThread> list_of_get_threads = new ArrayList<GETThread>() ;
+		int num_of_hosts_get = 0 ;
+		
+		for ( HashMap.Entry<UdpAddress, ArrayList<OID>> entry : map.entrySet() ) {
+			
+			GETThread get = new GETThread( entry.getKey(), entry.getValue()) ;
+			list_of_get_threads.add(get) ;
+			num_of_hosts_get++ ;
+			
+		}
 		
 		UdpAddress udp_trap = new UdpAddress("0.0.0.0/162") ;
-		TRAPThread trap = new TRAPThread( udp_trap, get) ;
+		TRAPThread trap = new TRAPThread( udp_trap, list_of_get_threads.get(0)) ;
 		
-		executor.execute(get);
+		ExecutorService executor = Executors.newFixedThreadPool(num_of_hosts_get+1) ; //numero di host per le GET e thread per ricevere TRAPS
+		
+		for ( int i = 0 ; i < num_of_hosts_get ; i++ ) {
+			
+			executor.execute(list_of_get_threads.get(i));
+			
+		}
+		
 		executor.execute(trap);
 		
 	}
@@ -30,24 +49,43 @@ public class ManagerServerExecutor {
 		
 		OID_OIC_Mapping = new HashMap() ;
 		OID_Text_Mapping = new HashMap() ;
+		IP_OID_Get_Mapping = new HashMap() ;
 		
 		String line = "" ;
 		String splitBy = "," ;
+		String [] infos ;
+		ArrayList<OID> OID_list = new ArrayList<OID>() ;
 		
 		try {
 			
-			BufferedReader br = new BufferedReader(new FileReader("db.csv")) ;
+			//BufferedReader br = new BufferedReader(new FileReader("db.csv")) ;
+			BufferedReader br2 = new BufferedReader(new FileReader("get.csv")) ;
 			
-			while ( ( line = br.readLine()) != null ) {
+			/*while ( ( line = br.readLine()) != null ) {
 				
-				String [] infos = line.split(splitBy) ;
+				infos = line.split(splitBy) ;
 				try {
 				OID_OIC_Mapping.put(infos[0],infos[3]) ;
 				OID_Text_Mapping.put(infos[0],infos[1]) ;	
 				} catch( ArrayIndexOutOfBoundsException e ) {} 
+			}*/
+			
+			while ( ( line = br2.readLine()) != null ) {
+				
+				infos = line.split(splitBy) ;
+				OID_list.clear();
+				
+				if ( infos.length > 1 ) { 
+					for ( int i = 1 ; i < infos.length ; i++ ) {
+					
+						OID temp_oid = new OID(infos[i]) ;
+						OID_list.add(temp_oid) ;
+					}
+					
+				}
+				UdpAddress temp = new UdpAddress(infos[0]) ;
+				IP_OID_Get_Mapping.put(temp, OID_list) ;
 			}
-
-			System.out.println(OID_OIC_Mapping);
 			
 		} catch( IOException e ) {
 			
@@ -56,4 +94,11 @@ public class ManagerServerExecutor {
 		}
 
 	}
+	
+	public HashMap<UdpAddress, ArrayList<OID>> getIP_OID_Get_Mapping() {
+		return IP_OID_Get_Mapping;
+	}
+	
+	
 }
+
